d595d536511338254ebbc477653eb514a3a1c5ad
GaspardLemarque <gleb.ushakov@yahoo.com>
2023-07-01 12:25:27 +0200

    Add parser to the app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request
 import plotly.graph_objects as go
 from fredapi import Fred
+import parser
 
 # Parse an .env file
 load_dotenv()
@@ -19,12 +20,16 @@ def index():
 
 @app.route('/plot', methods=['POST'])
 def plot():
-    series_id = request.form['series_id']
-    data = fred.get_series(series_id)
+    if 'series_id' in request.form:
+        # Handle the request from the first form
+        series_id = request.form['series_id']
+        fred_data = fred.get_series(series_id)
 
-    if data is not None:
-        # Create a Plotly figure
-        fig = go.Figure(data=go.Scatter(x=data.index, y=data))
+        if fred_data is None:
+            return "Failed to retrieve data from the FRED API."
+
+        # Create a Plotly figure with updated layout using API data
+        fig = go.Figure(data=go.Scatter(x=fred_data.index, y=fred_data))
 
         fig.update_layout(
             title=series_id,
@@ -32,14 +37,34 @@ def plot():
             yaxis=dict(title='Value'),
             showlegend=False
         )
+    elif 'column_name' in request.form:
+        # Handle the request from the second form
+        column_name = request.form['column_name']
+        ascii_data = parser.df
 
-        # Convert the figure to JSON for rendering in the template
-        fig_json = fig.to_json()
+        if ascii_data is None:
+            return "Failed to retrieve data for the plot."
 
-        # Pass the figure JSON to the template for rendering
-        return render_template('plot.html', fig_json=fig_json)
+        if column_name not in ascii_data.columns:
+            return "Invalid column name."
+
+        # Create a Plotly figure with updated layout using dataframe column
+        fig = go.Figure(data=go.Scatter(x=ascii_data.index, y=ascii_data[column_name]))
+
+        fig.update_layout(
+            title=column_name,
+            xaxis=dict(title='Date'),
+            yaxis=dict(title='Value'),
+            showlegend=False
+        )
     else:
-        return "Failed to retrieve data from the FredAPI."
+        return "Invalid form submission."
+
+    # Convert the figure to JSON for rendering in the template
+    fig_json = fig.to_json()
+
+    # Pass the figure JSON to the template for rendering
+    return render_template('plot.html', fig_json=fig_json)
 
 
 # Run server
